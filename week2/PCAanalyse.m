57eca2b2305cca4130e3fe8f2cb7643a33e7c684
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2014-03-03 18:44:21 +0100

    First commit

---
+++ week2/PCAanalyse.m
@@ -0,0 +1,11 @@
+function [ mu, lambda, U ] = PCAanalyse( X )
+%PCAANALYSE Summary of this function goes here
+%   Detailed explanation goes here
+
+% Get mean for each of the d rows
+mu = mean(X, 1);
+
+[U, lambda] = eig(X);
+
+end
+

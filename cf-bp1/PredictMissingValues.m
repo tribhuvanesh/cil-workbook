384c11ec2e4d59f50d7605d805607193f45af5dd
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2014-06-04 14:38:30 +0200

    Added kmeans+SVD approach; Optimized vanilla SVD approach

--- cf-bp1/PredictMissingValues.m
+++ cf-bp1/PredictMissingValues.m
@@ -2,8 +2,11 @@ function X_pred = PredictMissingValues(X, nil)
 % Predict missing entries in matrix X based on known entries. Missing
 % values in X are denoted by the special constant value nil.
 
+% Rows contain users, column contains items
+
 %% Parameters
-k = 5;
+SVD_K = 6;
+KMEANS_K = 2;
 
 %% 0. Preprocessing
 % a. Calculate the mean of all observed ratings
@@ -16,28 +19,34 @@ mu = mean(mean(X_pred(X_pred ~= nil)));
 
 % Replace missing ratings with average ratings by that user
 X_pred_avg = zeros(size(X_mean_imputed, 1), 1);
+X_for_kmeans = X;
 for i=1:size(X_mean_imputed, 1)
-    xi = X_pred(i, :);
+    xi = X(i, :);
     X_pred_avg(i) = mean(xi(xi ~= nil));
     xi(xi == nil) = X_pred_avg(i);
     X_mean_imputed(i, :) = xi;
+    X_for_kmeans(i, :) = X_mean_imputed(i, :) - X_pred_avg(i);
 end
 
-%% 2. Train
-% Train the model
+%% K-means and SVD
+% Apply k-means to cluster similar users
+kmeans_idx = kmeans(X_for_kmeans, KMEANS_K);
 
-[U, D, V] = svd(X_mean_imputed, 0);
+% For each cluster, perform SVD and impute missing values
+for i=1:KMEANS_K
+    % Obtain all users belonging to cluster i
+    X_org_k = X(kmeans_idx == i, :);
+    X_mean_imputed_k = X_mean_imputed(kmeans_idx == i, :);
 
-U_trunc = U(:, 1:k);
-D_trunc = D(1:k, 1:k);
-V_trunc = V(:, 1:k);
-
-U_p = U_trunc * sqrt(D_trunc);
-V_p = sqrt(D_trunc) * V_trunc';
-X_svd = U_trunc * D_trunc * V_trunc';
-
-
-%% 3. Predict
-% Replaced unknown values in X_pred with new values
-X_pred(X_pred == nil) = X_svd(X_pred == nil);
+    [U, D, V] = svd(X_mean_imputed_k, 0);
+    U_trunc = U(:, 1:SVD_K);
+    D_trunc = D(1:SVD_K, 1:SVD_K);
+    V_trunc = V(:, 1:SVD_K);
+    X_svd_k = U_trunc * D_trunc * V_trunc';
+    
+    % Replace, when possible, imputed SVD values with known ratings
+    % X_svd_k(X_org_k ~= nil) = X_org_k(X_org_k ~= nil);
+    % Replace unknown ratings with SVD imputed values
+    X_pred(kmeans_idx == i, :) = X_svd_k;
+end
 
--- week3/PredictMissingValues.m
+++ week3/PredictMissingValues.m
@@ -18,13 +18,13 @@ for i=1:size(X_pred_copy, 1)
 end
 
 % SVD solution
-[U, D, V] = svd(X_pred_copy);
+[U, D, V] = svd(X_pred_copy, 0);
 % k = 100; % 4.653
 % k = 50; % 4.5737
 % k = 25; % 4.4412
 % k = 10; % 4.3754
 % k = 7; % 4.3593
-k = 5; % 4.357
+k = 6; % 4.357
 % k = 4; % 4.3651
 % k = 2; % 4.4155
 
@@ -32,19 +32,8 @@ U_trunc = U(:, 1:k);
 D_trunc = D(1:k, 1:k);
 V_trunc = V(:, 1:k);
 
-size(U_trunc)
-size(D_trunc)
-size(V_trunc)
-
 U_p = U_trunc * sqrt(D_trunc);
 V_p = sqrt(D_trunc) * V_trunc';
 X_pred_copy = U_trunc * D_trunc * V_trunc';
 
-for i=1:size(X_pred, 1)
-    for j=1:size(X_pred, 2)
-        if X_pred(i, j) == nil
-            % X_pred(i, j) = U_p(i) * V_p(j);
-            X_pred(i, j) = X_pred_copy(i, j);
-        end
-    end
-end
+X_pred(X_pred == nil) = X_pred_copy(X_pred == nil);

a3b7fb5fb7b1ea58d786fb5496e2891c556e5a95
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2014-06-05 11:22:01 +0200

    Adding SGD variant

--- cf-korbell-ot/PredictMissingValues.m
+++ cf-korbell-ot/PredictMissingValues.m
@@ -1,123 +1,41 @@
-
-
 function X_pred = PredictMissingValues(X, nil)
 % Predict missing entries in matrix X based on known entries. Missing
 % values in X are denoted by the special constant value nil.
 
-% your collaborative filtering code here!
-%X_pred = stdize(X);
-X_pred = StochasticSVD(X, nil);
-end
-function Y = stdize(X)
- Y = zeros(size(X));
- Y = (X - repmat(mean(X),size(X,1),1))./repmat(std(X), size(X,1), 1);
-end
+%% Parameters
+% Parameters and stuff
 
-function [X_pred, U, V] = StandardSVD(X_pred, X, nil)
-num_users = size(X_pred,1);
-num_movies = size(X_pred, 2);
-avg =zeros(num_movies,1);
-offset = zeros(num_users,1);
-br = 10; %blending ratio
-observed = zeros(num_movies,1);  
-rated = zeros(num_users,1);
+DIM_F = 100;
+NUM_USERS = size(X, 1);
+NUM_ITEMS = size(X, 2);
 
-for i= 1:size(X_pred,2)
-    curr_col = X_pred(:, i);
-    avg(i) = mean(curr_col(curr_col~=nil));
-    len = size(curr_col(curr_col~=nil),1);
-    observed(i) = len;
-end
+GAMMA = 0.001;
+LAMBDA = 1.5;
 
-global_average = mean(avg)
-%Calculate better average
-for i = 1:size(avg)
-    r = (br*global_average + avg(i) *observed(i))/ (br + observed(i));
-    avg(i) = r;
-end    
-for i= 1:size(X_pred,1)
-    counter = 0;
-    o =0;
-    for j = 1:num_movies
-          if X_pred(i,j)~= nil
-              counter = counter + 1;
-              o = o + X_pred(i, j) - avg(j);     
-          end    
-    end
-    rated(i) = counter;
-    offset(i) = o/counter;
-end
+%% Preprocessing
+% Preprocessing stuff
 
-offset_average = mean(offset);
-for i = 1:size(offset)
-    r = (br*offset_average + offset(i) *rated(i))/ (br + rated(i));
-    offset(i) = r;
-end   
-%Calculate better offset averages
+global_mean = mean(X(X ~= nil));
+% Each row contains p_u
+P = rand(NUM_USERS, DIM_F);
+% Each row contains q_i
+Q = rand(NUM_ITEMS, DIM_F);
+% Error matrix
+% E = zeros(NUM_USERS, NUM_ITEMS);
 
-
-for i = 1:size(X_pred,1)
-    for j= 1:size(X_pred,2)
-        if(X(i,j) == nil)
-            X_pred(i,j) = avg(j) + offset(i);
+%% Perform SGD
+% Perform Stochastic Gradient Descent
+for u=1:NUM_USERS
+    for i=1:NUM_ITEMS
+        if X(u, i) ~= nil
+            e_ui = X(u, i) - Q(i, :)*P(u, :)';
+            temp1 = Q(i, :) + GAMMA * (e_ui*P(u, :) - LAMBDA*Q(i, :));
+            temp2 = P(u, :) + GAMMA * (e_ui*Q(i, :) - LAMBDA*P(u, :));
+            Q(i, :) = temp1;
+            P(u, :) = temp2;
         end
     end
 end
 
-
-m = mean(X_pred);
-s  = std(X_pred); 
-X_pred = stdize(X_pred);
-k = 8;
-lambda = 125;
-[U,S,V] = svd(X_pred,0);
-I = eye(size(S,1));
-S = S + I * lambda;
-U = U * sqrt(S);
-U = U(:,1:k);
-V =  sqrt(S)* V ;
-V = V(:,1:k);
-V = V';
-for i = 1:size(X_pred,1)
-    for j= 1:size(X_pred,2)
-        if(X(i,j) == nil)
-            X_pred(i,j) = U(i,:) * V(:,j);
-        end
-    end
-end
-%unstandardize
-X_pred = X_pred.*repmat(s, size(X,1), 1);
-X_pred = (X_pred + repmat(m,size(X,1),1));
-end
-
-
-function X_pred = StochasticSVD(X, nil)
-X_pred = X;
-[row,col] = find(X~=nil);
-average = mean(X(X~=nil));
-learningRate = 0.005;
-regularizer = 0.02;
-%numFeatures = 8 ;
-[X_pred, Users, Movies] = StandardSVD(X_pred, X, nil);
-for i = 1:2
-for u = 1:size(Users, 1)
-    for m = 1:size(Movies, 2)
-         if(X(u, m) ~= nil)     
-            rating = average + Users(u,:) * Movies(:,m);
-            %rating
-            error = (X(u, m) - rating);
-            Movies(:,m) = Movies(:,m) + learningRate *( error * Users(u,:)' - regularizer * Movies(:,m));
-            Users(u,:) = Users(u,:) + learningRate *( error * Movies(:,m)' - regularizer * Users(u,:));
-         end  
-    end
-end
-end
-
-for i = 1:size(X_pred,1)
-    for j= 1:size(X_pred,2)
-        if(X(i,j) == nil)
-            X_pred(i,j) = average +  Users(i,:) * Movies(:,j);
-        end
-    end
-end
-end
+X_pred = P*Q';
+% X_pred(X ~= nil) = X(X ~= nil);
